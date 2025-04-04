62c7b053b200db16ccd6e7117f792d4b5abef829
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-04-04 13:35:05 -0400

    set up basic home.js layout

---
+++ src/modules/home.js
@@ -0,0 +1,13 @@
+export default funciton loadHome(){
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+
+    const headline = document.createElement("h1");
+    headline.textContent = "Welcome to My Restaurant";
+
+    const description = document.createElement("p");
+    description.textContent = "The best place for delicious food";
+    
+    content.appendChild(headline);
+    content.appendChild(description);
+}
