c1e00f9660e2d8731624b13b29d1508ef1516cf9
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-04-07 13:22:36 -0400

    switched home image and description elements in home.js

--- src/modules/home.js
+++ src/modules/home.js
@@ -7,15 +7,16 @@ export default function loadHome() {
   const headline = document.createElement("h1");
   headline.textContent = "Welcome to Our Restaurant";
 
-  const description = document.createElement("p");
-  description.textContent = "The best place for delicious food.";
-
   const image = document.createElement("img");
   image.src = insideRestaurantImage;
   image.alt = "Inside Restaurant";
   image.classList.add("restaurant-image");
 
+  const description = document.createElement("p");
+  description.textContent = "At [Restaurant Name], we're passionate about creating memorable dining experiences. Whether you're looking for a casual night out with friends, a romantic dinner, or a special occasion celebration, we're dedicated to providing a warm and inviting atmosphere where you can relax and enjoy delicious food";
+  description.classList.add("home-page-description");
+
   content.appendChild(headline);
-  content.appendChild(description);
   content.appendChild(image);
+  content.appendChild(description);
 }
